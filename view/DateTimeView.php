677836c9f9c061eece23357812ac78c01d2e9346
jimmybengtsson <jb223pu@student.lnu.se>
2017-09-15 15:57:39 +0200

    Fixed day of month.

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -2,12 +2,32 @@
 
 class DateTimeView {
 
+    private function checkDayOfMonth($monthDay) {
+
+        if ($monthDay == 1 || $monthDay == 21 || $monthDay == 31)
+        {
+            return $monthDay . 'st';
+        }
+        else if ($monthDay == 2 || $monthDay == 22)
+        {
+            return $monthDay . 'nd';
+        }
+        else if ($monthDay == 3 || $monthDay == 23)
+        {
+            return $monthDay . 'rd';
+        }
+        else
+        {
+            return $monthDay . 'th';
+        }
+    }
+
 
 	public function show() {
 
         $today = getdate();
 
-		$timeString = $today['weekday'] . ', the ' . $today['mday'] . 'th of ' . $today['month'] . ' ' .
+		$timeString = $today['weekday'] . ', the ' . $this->checkDayOfMonth($today['mday']) . ' of ' . $today['month'] . ' ' .
             $today['year'] . ', The time is ' . date("H:i:s");
 
 		return '<p>' . $timeString . '</p>';
